45e2c826f2bd4192415b4a3884f11363c6f9f121
Guillaume Malleret <guillaume.malleret@flex-multimedia.com>
2014-08-15 11:04:03 +0200

    reformatage

--- lib/Flex/RestClient/ClientCollection.php
+++ lib/Flex/RestClient/ClientCollection.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Flex\RestClient;
 
 /**
@@ -10,21 +11,21 @@ Class ClientCollection extends AbstractCollection {
 	protected $mh;
 
 	/**
-     * Sets a Client in the client collection at the specified key/index.
-     *
-     * @param string|integer $key   The key/index of the element to set.
-     * @param \Flex\RestClient\Client $client Client
-     */
+	 * Sets a Client in the client collection at the specified key/index.
+	 *
+	 * @param string|integer $key   The key/index of the element to set.
+	 * @param \Flex\RestClient\Client $client Client
+	 */
 	public function add(Client $client) {
 		$this->collection[] = $client;
 	}
 
 	/**
-     * Sets a Client in the collection at the specified key/index.
-     *
-     * @param string|integer $key   The key/index of the element to set.
-     * @param \Flex\RestClient\Client $client Client
-     */
+	 * Sets a Client in the collection at the specified key/index.
+	 *
+	 * @param string|integer $key   The key/index of the element to set.
+	 * @param \Flex\RestClient\Client $client Client
+	 */
 	public function set($key, Client $client) {
 		$this->collection[$key] = $client;
 	}
@@ -36,49 +37,47 @@ Class ClientCollection extends AbstractCollection {
 
 		$this->mh = curl_multi_init();
 
-		foreach($this->collection as $key => $client){
+		foreach ($this->collection as $key => $client) {
 			$client->init();
 			curl_multi_add_handle($this->mh, $client->getCurlHandler());
 		}
-
 	}
 
-	public function execute(){
+	public function execute() {
 
 		$this->init();
 
 		$active = null;
 		$selectTimeout = 0.001;
-			
+
 		//execute the handles
 		do {
 			$status = curl_multi_exec($this->mh, $active);
 
 			if ($active && curl_multi_select($this->mh, $selectTimeout) === -1) {
-                // Perform a usleep if a select returns -1: https://bugs.php.net/bug.php?id=61141
-                usleep(150);
-            }
+				// Perform a usleep if a select returns -1: https://bugs.php.net/bug.php?id=61141
+				usleep(150);
+			}
 		} while ($status === CURLM_CALL_MULTI_PERFORM || $active);
 
 		$responses = new ResponseCollection();
 
-		foreach($this->collection as $key => $client){
+		foreach ($this->collection as $key => $client) {
 			$responses[$key] = new Response(curl_multi_getcontent($client->getCurlHandler()),
 											curl_getinfo($client->getCurlHandler()),
 											curl_error($client->getCurlHandler()));
 		}
 
 		return $responses;
-		
 	}
 
 	//close the handles
 	public function __destruct() {
 
-		if(empty($this->mh))
+		if (empty($this->mh))
 			return;
-		
-		foreach($this->collection as $key => $client){
+
+		foreach ($this->collection as $key => $client) {
 			curl_multi_remove_handle($this->mh, $client->getCurlHandler());
 		}
 
--- lib/Flex/RestClient/ResponseCollection.php
+++ lib/Flex/RestClient/ResponseCollection.php
@@ -1,23 +1,24 @@
 <?php
+
 namespace Flex\RestClient;
 
 Class ResponseCollection extends AbstractCollection {
 
 	/**
-     * Sets a Response in the collection at the specified key/index.
-     *
-     * @param \Flex\RestClient\Response $response Response
-     */
-	public function add( Response $response) {
+	 * Sets a Response in the collection at the specified key/index.
+	 *
+	 * @param \Flex\RestClient\Response $response Response
+	 */
+	public function add(Response $response) {
 		$this->collection[] = $response;
 	}
 
 	/**
-     * Sets a Response in the collection at the specified key/index.
-     *
-     * @param string|integer $key   The key/index of the element to set.
-     * @param \Flex\RestClient\Response $response Response
-     */
+	 * Sets a Response in the collection at the specified key/index.
+	 *
+	 * @param string|integer $key   The key/index of the element to set.
+	 * @param \Flex\RestClient\Response $response Response
+	 */
 	public function set($key, Response $response) {
 		$this->collection[$key] = $response;
 	}
