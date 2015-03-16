1cdad3ba3f76cd599fb9f517160b5b83bd95316f
Guillaume Malleret <guillaume.malleret@flex-multimedia.com>
2015-03-16 15:20:16 +0100

    correction setTimeout

--- lib/Flex/RestClient/Client.php
+++ lib/Flex/RestClient/Client.php
@@ -265,7 +265,7 @@ Class Client
 	}
 
 	public function setTimeout($timeout) {
-		if(is_numeric($timeout)) {
+		if(!is_numeric($timeout)) {
 			throw new \InvalidArgumentException('timeout numeric expected');
 		}
 		$this->timeout = $timeout;
